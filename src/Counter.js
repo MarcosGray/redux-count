34f2365ed1482b802ccfcfa99d71c2342487e892
Marcos Gray <marcosgray@gmail.com>
2019-01-18 14:47:20 -0300

    Initial commit of redux-count project

---
+++ src/Counter.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { increment, decrement } from './actions'
+
+export const Counter = ({count, increment, decrement}) => {
+    return (
+        <p>
+            Contador: 
+            <span className='counter' >{count}</span>
+            <button className='increment' onClick={() => increment(20)}>+</button>
+            <button className='decrement' onClick={() => decrement(15)}>-</button>
+        </p>
+    )
+}
+const mapStateToProps = (state) => {
+    return {
+        count: state.count
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increment: (value) => dispatch(increment(value)),
+        decrement: (value) => dispatch(decrement(value))
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps) (Counter)
